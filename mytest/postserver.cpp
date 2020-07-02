75b3d916ea1f05fa550298e7ee5be59037f339e4
libeio <ytmrlimei@163.com>
2020-07-02 12:24:04 +0800

    continue to add test file

--- mytest/postserver.cpp
+++ mytest/postserver.cpp
@@ -1,21 +1,25 @@
-/**
- * 测试方法:
- *  curl -w'\n' -v -X GET http://localhost:1984/resource/libei
- */
 
+#include <stdio.h>
 #include <string>
 #include <memory>
 #include <iostream>
 #include <restbed>
 
-void get_method_handler(const std::shared_ptr<restbed::Session> session)
+#include "header.h"
+
+void post_method_handler(const std::shared_ptr<restbed::Session> session)
 {
     const auto& request = session->get_request();
     
-    std::cout << "my name=" << request->get_header("MyName") << std::endl;
-    std::cout << "index=" << request->get_header("index", 0) << std::endl;
+    std::cout << "name="    << request->get_header("MyName")
+              << ", index=" << request->get_header("Index", 0)
+              << " | ";
     
-    const std::string body = "Hello, " + request->get_path_parameter("name");
+    unsigned char buf[32]{0};
+    randomX4(buf, 32);
+    
+    const std::string body = std::string(buf, buf + 32);
+    std::cout << bin2hex(body) << std::endl;
         
     session->yield(restbed::OK, body, { { "Content-Length", std::to_string(body.size()) },
                                         { "Connection", "keep-alive" } } );
@@ -25,7 +29,7 @@ int main( const int, const char** )
 {
     auto resource = std::make_shared<restbed::Resource>();
     resource->set_path("/resource/{name: .*}");
-    resource->set_method_handler("GET", get_method_handler);
+    resource->set_method_handler("POST", post_method_handler);
     
     auto settings = std::make_shared<restbed::Settings>();
     settings->set_port(1984);
